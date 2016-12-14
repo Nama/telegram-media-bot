a700f8855490e3be902b4411c3d943f8a4ef1d3e
Nama <mrt690@gmail.com>
2016-12-14 23:57:04 +0100

    Make package and fix double posts
    double posts on non-direct links are fixed
    added requirements.txt
    made package with setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='telegram-media-bot',
+    version="2.1",
+    packages=find_packages(),
+    scripts=['telegram-media-bot/telegram-media-bot.py'],
+
+    install_requires=['tweepy>=3.5.0', 'praw<=3.6.0', 'requests>=2.12.3'],
+
+    package_data={
+        'telegram-media-bot': ['requirements.txt', 'config.json'],
+    },
+
+    author='Murat Özel',
+    description='Send pictures and videos from Twitter and Reddit to a Telegram-Chat',
+    license='MIT',
+    keywords='reddit telegram twitter',
+    url="https://blog.yamahi.eu",
+)
